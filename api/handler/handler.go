35a2ba8275f78e32d2f9b5b41ddfe1164d19a7ab
Ozodbek <ozodbekshamsiddinov1504@gmail.com>
2026-03-17 09:51:05 +0500

    genproto add and handler is fixxed

--- api/handler/handler.go
+++ api/handler/handler.go
@@ -1,1 +1,25 @@
 package handler
+
+import (
+	"gateway/genproto/ishlab_chiqarish"
+	"gateway/genproto/user"
+	"log/slog"
+
+	"github.com/casbin/casbin/v2"
+)
+
+type Handler struct {
+	UserService user.UserServiceClient
+	Log         *slog.Logger
+	DocsService ishlab_chiqarish.IshlabChiqarishServiceClient
+	Enforcer    *casbin.Enforcer
+}
+
+func NewHandler(user user.UserServiceClient, production ishlab_chiqarish.IshlabChiqarishServiceClient, logger *slog.Logger, Enforcer *casbin.Enforcer) *Handler {
+	return &Handler{
+		UserService: user,
+		Log:         logger,
+		DocsService: production,
+		Enforcer:    Enforcer,
+	}
+}
--- go.mod
+++ go.mod
@@ -1,8 +1,21 @@
 module gateway
 
-go 1.23.4
+go 1.24.0
 
 require (
+	github.com/casbin/casbin/v2 v2.135.0
 	github.com/joho/godotenv v1.5.1
 	github.com/spf13/cast v1.10.0
+	google.golang.org/grpc v1.79.2
+	google.golang.org/protobuf v1.36.11
+)
+
+require (
+	github.com/bmatcuk/doublestar/v4 v4.6.1 // indirect
+	github.com/casbin/govaluate v1.3.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	golang.org/x/net v0.48.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/text v0.32.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251202230838-ff82c1b0f217 // indirect
 )
